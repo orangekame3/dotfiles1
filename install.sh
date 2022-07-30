8fd3808924ccca668f0cfe9e66758de68a90bb33
orangekame3 <miya.org.0309@outlook.jp>
2022-07-30 23:43:06 +0900

    update

---
+++ install.sh
@@ -0,0 +1,14 @@
+# #!/bin/zsh
+
+# zsh
+sudo apt install zsh
+# [satarship]
+install_starship() {
+    # Automatically install/update starship
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --bin-dir ~/.local/bin --yes
+}
+
+# Automatically install starship if not exists
+if ! type starship >/dev/null; then
+    install_starship
+fi
