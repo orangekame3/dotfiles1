adc38f39bd448abb14071d2f2b984e62d4414cb6
orangekame3 <miya.org.0309@outlook.jp>
2022-07-31 01:42:19 +0900

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt install zsh
 sudo apt install tree
 wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+rm go1.18.4.linux-amd64.tar.gz
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
