5f98e1c0c535722bc6aa2a69d38b11669777786c
orangekame3 <miya.org.0309@outlook.jp>
2022-07-31 01:03:09 +0900

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 # zsh
 sudo apt install zsh
-chsh -s /usr/bin/zsh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 zsh
@@ -16,3 +15,4 @@ install_starship() {
 if ! type starship >/dev/null; then
     install_starship
 fi
+chsh -s /usr/bin/zsh
